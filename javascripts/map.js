39813adfdce1b87f2b075bf0f2e20d16573496e1
Paul Han <paulhan221@gmail.com>
2015-05-13 11:21:39 -0400

    implemented distance matrix

--- javascripts/map.js
+++ javascripts/map.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
   var map;
   var address;
   var schools;
+  var bounds = new google.maps.LatLngBounds();
+  var markersArray = [];
+  
+  // marker icons
+  var destinationIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=D|FF0000|000000';
+  var originIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=O|FFFF00|000000';
 
   function initialize() {
     geocoder = new google.maps.Geocoder();
@@ -13,10 +19,9 @@ $(document).ready(function() {
     }
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   }
-
+  // start code address //
   function codeAddress() {
     address = document.getElementById('address').value;
-
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
@@ -28,16 +33,94 @@ $(document).ready(function() {
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
-    // get sample data
+    // start calculate distance //
+    function calculateDistances() {
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [address],
+          destinations: ["14094"],
+          travelMode: google.maps.TravelMode.DRIVING,
+          unitSystem: google.maps.UnitSystem.METRIC,
+          avoidHighways: false,
+          avoidTolls: false
+        }, callback);
+    }
+    // end calculate distance //
+    $("#distanceCalc").click(function(){
+        calculateDistances();
+      });
+    //////////////// get sample data //////////////////////
     $.get( "http://lofischools.herokuapp.com/search?query=School&state=NY&limit=10", function(data) {
       schools = JSON.parse(data)["results"];
-    debugger;
+    });
+    ///////////////////end sample data////////////////////
+
+  }
+  // end code address //
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // start distance matrix functions
+  function callback(response, status) {
+    if (status != google.maps.DistanceMatrixStatus.OK) {
+      alert('Error was: ' + status);
+    } else {
+      var origins = response.originAddresses;
+      var destinations = response.destinationAddresses;
+      var outputDiv = document.getElementById('outputDiv');
+      outputDiv.innerHTML = '';
+      deleteOverlays();
+
+      for (var i = 0; i < origins.length; i++) {
+        var results = response.rows[i].elements;
+        addMarker(origins[i], false);
+        for (var j = 0; j < results.length; j++) {
+          addMarker(destinations[j], true);
+          outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
+              + ': ' + results[j].distance.text + ' in '
+              + results[j].duration.text + '<br>';
+        }
+      }
+    }
+  }
+
+  function addMarker(location, isDestination) {
+    var icon;
+    if (isDestination) {
+      icon = destinationIcon;
+    } else {
+      icon = originIcon;
+    }
+    geocoder.geocode({'address': location}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        bounds.extend(results[0].geometry.location);
+        map.fitBounds(bounds);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+          icon: icon
+        });
+        markersArray.push(marker);
+      } else {
+        alert('Geocode was not successful for the following reason: '
+          + status);
+      }
     });
   }
 
+  function deleteOverlays() {
+    for (var i = 0; i < markersArray.length; i++) {
+      markersArray[i].setMap(null);
+    }
+    markersArray = [];
+  }
+  // end distance matrix functions
+ ////////////////////////////////////////////////////////////////////////////////////////
+
   google.maps.event.addDomListener(window, 'load', initialize);
+
   // upon clicking zipcode, find on map
-  $("#codeZip").click(function(){
+  $("#geocodeZip").click(function(){
     codeAddress();
   });
 
