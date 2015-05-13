8d6c923c3946c532545defbbd277a4887070ce6d
Paul Han <paulhan221@gmail.com>
2015-05-13 16:23:11 -0400

    added steps

--- javascripts/map.js
+++ javascripts/map.js
@@ -44,9 +44,7 @@ $(document).ready(function() {
        // Try HTML5 geolocation
       if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = new google.maps.LatLng(position.coords.latitude,
-                                           position.coords.longitude);
-
+          var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           var infowindow = new google.maps.InfoWindow({
             map: map,
             position: pos,
