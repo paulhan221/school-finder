fca04cc21d2ec23441ece183d7d73f8ffb51b449
Paul Han <paulhan221@gmail.com>
2015-05-13 11:03:14 -0400

    made sample data available upon entering a zip code

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,50 +1,22 @@
 $(document).ready(function() {
-  var map;
-  var infowindow;
   var geocoder;
+  var map;
+  var address;
+  var schools;
 
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
-
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: pyrmont,
-    zoom: 15
-  });
-
-  var request = {
-    location: pyrmont,
-    radius: 500,
-    types: ['school']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
     }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
 
   function codeAddress() {
-    var address = document.getElementById("address").value;
+    address = document.getElementById('address').value;
+
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
@@ -53,15 +25,21 @@ function createMarker(place) {
             position: results[0].geometry.location
         });
       } else {
-        alert("Geocode was not successful for the following reason: " + status);
+        alert('Geocode was not successful for the following reason: ' + status);
       }
     });
+    // get sample data
+    $.get( "http://lofischools.herokuapp.com/search?query=School&state=NY&limit=10", function(data) {
+      schools = JSON.parse(data)["results"];
+    debugger;
+    });
   }
-$( "#img-click" ).click(function() {
-  codeAddress();
-});
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
+  // upon clicking zipcode, find on map
+  $("#codeZip").click(function(){
+    codeAddress();
+  });
 
 });
 
---
+++ javascripts/sampleData.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // var schools;
+
+  // $.get( "http://lofischools.herokuapp.com/search?query=School&state=NY&limit=10", function(data) {
+  //   schools = JSON.parse(data)["results"];
+  // });
+
+
+});
